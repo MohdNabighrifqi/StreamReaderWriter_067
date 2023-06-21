3a25d71965814ba83a7d74390369bbbca2f22d8e
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-06-21 11:45:50 +0700

    meelakukan perbaikan dan jika terjadi kesalahan maka yang di eksekusi di exception

--- ExceptionStandart/ExceptionStandart.cpp
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -9,9 +9,14 @@ int main()
 {
 	cout << "awal program" << endl;	//penanda 1...
 	try {
-		array <int, 3> data = { 1,2,3 };
+		array <int, 3> data = { 3, 5, 7 };
 		//pesan array integer 3 elemen
 		cout<<data.at(5)<<endl;
 		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 element*/
 	}
 	
