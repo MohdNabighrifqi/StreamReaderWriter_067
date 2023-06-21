d7cb573a9f4a15109e31a119c76b74d61643cea6
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-06-21 10:48:01 +0700

    menutup file

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -23,5 +23,8 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' kedalam file
 		outfile << baris << endl;
 	}
-	
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+
 }
